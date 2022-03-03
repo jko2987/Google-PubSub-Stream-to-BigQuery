528b829cd1a3d93117195e125dd998ce835d494a
jko2987 <54950781+jko2987@users.noreply.github.com>
2022-03-03 10:50:04 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,50 @@
+"""Push Pub / Sub message to BigQuery."""
+
+import base64
+import datetime
+import json
+from google.cloud import bigquery
+dataset_id = "your-gcp-project.your-dataset"
+
+
+def record_event_to_bq(event, context):
+  """Triggered from a message on a Cloud Pub/Sub topic."""
+
+  # retrieve attributes
+
+   # current time
+  ts = datetime.datetime.now()
+  ts_string = str(ts)
+
+  # retrieve message id
+  message_id = context.event_id
+
+  # retrieve and decode data from message
+  data = event["data"]
+  json_string = base64.urlsafe_b64decode(data)
+  print("The json_string is: ", str(json_string))
+
+  msg_data = json.loads(json_string)
+  print(str(msg_data))
+
+  # populate test table
+  rows_to_insert_test = [{
+    "Column1-BigQuery": msg_data["Column1-PubSub-message"],
+    "Column2-BigQuery": msg_data["Column2-PubSub-message"],
+    "Column3-BigQuery": msg_data["Column3-PubSub-message"],
+    "etc-BigQuery": msg_data["etc-PubSub-message"],
+  }]
+
+  table_id_test = "your-table-name"
+
+  # attempt insert of data into BigQuery
+  client_test = bigquery.Client()
+  table_test = client_test.get_table(dataset_id + "." + table_id_test)
+  errors_test = client_test.insert_rows(table_test, rows_to_insert_test)
+
+  if errors_test == []:
+    # no errors! success, do something
+    print("success")
+  else:
+    # errors!
+    print(errors_test)
